f85bf62a259bf3b451aed77a7e6d8b20695d719d
Hiyat_ho <wjsdpgml0727@naver.com>
2025-09-04 11:04:13 +0900

    Update prototype01.c

--- packet_capture/packet_capture/prototype01.c
+++ packet_capture/packet_capture/prototype01.c
@@ -1,22 +1,30 @@
 #include <stdio.h>
 #include <pcap.h>
 #include <winsock2.h>
-#include <windows.h>
 #include <stdint.h>
 #include <ctype.h>
 
 void init_winsock();
-pcap_t* open_capture_handle(const char* dev, char* errbuf, pcap_t* handle);
-pcap_if_t* choose_device(pcap_if_t* alldevs, pcap_if_t* d, pcap_t* handle, char errbuf[PCAP_ERRBUF_SIZE], int choice);
+pcap_t* open_capture_handle(const char* dev, char* errbuf, pcap_t* handle, int filter);
+const char* choose_device(pcap_if_t* alldevs, pcap_if_t* d, pcap_t* handle, char errbuf[PCAP_ERRBUF_SIZE], int choice);
 void analyze_ipv4_packet(const u_char* pkt_data, struct pcap_pkthdr* header, struct eth_header* eth);
-void capture_loop(pcap_t* handle, u_char* pkt_data, int re, struct pcap_pkthdr* header);
+void capture_loop(pcap_t* handle, int filter);
 void print_hex_ascii(const u_char* payload, int len);
+void analyze_ARP_packet(struct eth_header* eth, const unsigned char* pkt_data);
+int choose_filter(int filter);
 
 #define MAC_ADDR_FMT "%02X:%02X:%02X:%02X:%02X:%02X"
 #define MAC_ADDR_FMT_ARGS(addr) addr[0], addr[1], addr[2], addr[3], addr[4], addr[5]
 
 struct ip_header {
+	unsigned char version_ihl;
+	unsigned char tos;
+	unsigned short total_len;
+	unsigned short id;
+	unsigned short frag_off;
+	unsigned char ttl;
 	unsigned char  protocol;
+	unsigned short check;
 	unsigned int   saddr;
 	unsigned int   daddr;
 };
@@ -27,6 +35,20 @@ struct eth_header {
 	unsigned short type;
 };
 
+#pragma pack(push, 2)
+struct arp_header {
+	unsigned short htype;
+	unsigned short ptype;
+	unsigned char hlen;
+	unsigned char plen;      
+	unsigned short opcode;   
+	unsigned char sender_mac[6];
+	struct in_addr sender_ip;
+	unsigned char target_mac[6];
+	struct in_addr target_ip;
+};
+#pragma pack(pop)
+
 int main(void)
 {
 	init_winsock();
@@ -35,11 +57,14 @@ int main(void)
 	pcap_if_t* d;
 	pcap_t* handle;
 	char errbuf[PCAP_ERRBUF_SIZE];
-	int choice = 0;
+	int choice = 0, filter = 0;
 
 	const char* dev = choose_device(&alldevs, &d, &handle, errbuf, choice);
+	
+	filter = choose_filter(filter);
 
-	handle = open_capture_handle(dev, errbuf, handle);
+	handle = open_capture_handle(dev, errbuf, handle, filter);
+	
 
 	WSACleanup();
 
@@ -49,18 +74,18 @@ int main(void)
 void init_winsock()
 {
 	WSADATA wsaData;
-	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 		printf("Winsock 초기화 실패\n");
-	}
 }
 
-pcap_t* open_capture_handle(const char* dev, char* errbuf, pcap_t* handle)
+pcap_t* open_capture_handle(const char* dev, char* errbuf, pcap_t* handle, int filter)
 {
-	struct pcap_pkthdr* header;
-	const u_char* pkt_data;
-	int re = 0;
-
 	handle = pcap_open_live(dev, 65535, 1, 1000, errbuf);
+
+	struct pcap_pkthdr* header = NULL;
+	const u_char* pkt_data = NULL;
+	int re = pcap_next_ex(handle, &header, &pkt_data);
 
 	if (handle == NULL)
 	{
@@ -68,36 +93,84 @@ pcap_t* open_capture_handle(const char* dev, char* errbuf, pcap_t* handle)
 		return NULL;
 	}
 
-	while (1)
-		capture_loop(handle, &pkt_data, re, &header);
-
+	capture_loop(handle, filter);
+	
 	pcap_close(handle);
+
+	return handle;
 }
 
-void capture_loop(pcap_t* handle, u_char* pkt_data, int re, struct pcap_pkthdr* header)
+void capture_loop(pcap_t* handle, int filter)
+{
+	struct pcap_pkthdr* header;
+	const u_char* pkt_data;
+
+	while (1)
+	{
+		int re = pcap_next_ex(handle, &header, &pkt_data);
+		//PCAP_API int	pcap_next_ex(pcap_t *, struct pcap_pkthdr **, const u_char **);
+
+		if (re != 1)
+			continue;
+
+		const u_char* packet = pkt_data;
+		int packet_len = header->len;
+
+		struct eth_header* eth = (struct eth_header*)pkt_data;
+		if ( (filter == 1) && (ntohs(eth->type) == 0x0800))
+		{
+			analyze_ipv4_packet(pkt_data, header, eth);
+			print_hex_ascii(packet, packet_len);
+		}
+
+		if ((filter == 2) && (ntohs(eth->type) == 0x0806))
+		{
+			analyze_ARP_packet(eth, packet);
+
+		}
+	}
+}
+
+void analyze_ARP_packet(struct eth_header* eth, const unsigned char* pkt_data)
 {
 	printf("\n--------------------------------------------------------------------\n\n");
-	re = pcap_next_ex(handle, &header, &pkt_data);
-	if (re == 0)
-		return 0;
 
-	struct eth_header* eth = (struct eth_header*)pkt_data;
-	if (ntohs(eth->type) == 0x0800)
-		analyze_ipv4_packet(pkt_data, header, eth);
-	else if (ntohs(eth->type) == 0x0806)
-		printf("ARP 패킷입니다.\n");
-	else if (ntohs(eth->type) == 0x86DD)
-		printf("IPv6 패킷입니다.\n");
-	else
-		printf("알 수 없는 Ethernet 타입: 0x%04X\n", ntohs(eth->type));
+	//ARP출력은 출발지/목적지가 아닌 보낸이(Sender)와 대상(Target)으로출력해야힘
+	struct arp_header* arp = (struct arp_header*)(pkt_data + 14);
+	unsigned short opcode = ntohs(arp->opcode);
 
-	const u_char* packet = pkt_data;
-	int packet_len = header->len;
-	print_hex_ascii(packet, packet_len);
+	if (opcode == 0x0001) {  // request = 1
+		printf(" ******* request ******* \n");
+		printf(" Sender IP : %s\n ", inet_ntoa(arp->sender_ip));  // 바이트 순서의 32비트 값을 주소값으로 변환하기 위함(in_addr 필요)
+		printf("Target IP : %s\n ", inet_ntoa(arp->target_ip));
+		printf("\n");
+	}
+
+	if (opcode == 0x0002) {  // reply = 2
+		printf(" ********  reply  ******** \n");
+		printf(" Sender IP  : %s\n ", inet_ntoa(arp->sender_ip));
+
+		printf("Sender MAC : ");
+		for (int i = 0; i <= 5; i++)
+			printf("%02x:", arp->sender_mac[i]);
+		printf("\n");
+
+		printf("\nTarget IP  : %s\n ", inet_ntoa(arp->target_ip));
+
+		printf("\nDst MAC : ");
+		for (int i = 0; i <= 5; i++)
+			printf("%02x:", arp->target_mac[i]);
+
+		printf("\n\n");
+	}
 }
 
 void analyze_ipv4_packet(const u_char* pkt_data, struct pcap_pkthdr* header, struct eth_header* eth)
 {
+	printf("\n--------------------------------------------------------------------\n\n");
+
+	printf("\n====== IPv4 packet ======\n");
+
 	printf("IPv4 패킷 길이: %d\n", header->len);
 
 	struct ip_header* ip = (struct ip_header*)(pkt_data + 14);
@@ -110,15 +183,13 @@ void analyze_ipv4_packet(const u_char* pkt_data, struct pcap_pkthdr* header, str
 	printf("프로토콜: %d\n\n", ip->protocol);
 }
 
-pcap_if_t* choose_device(pcap_if_t* alldevs, pcap_if_t* d, pcap_t* handle, char errbuf[PCAP_ERRBUF_SIZE], int choice )
+const char* choose_device(pcap_if_t* alldevs, pcap_if_t* d, pcap_t* handle, char errbuf[PCAP_ERRBUF_SIZE], int choice)
 {
 	printf("\n****************************************************************************\n\n");
 
 	if (pcap_findalldevs(&alldevs, errbuf) == -1)
-	{
+
 		printf("장치 검색 실패: %s\n", errbuf);
-		return 1;
-	}
 
 	int i = 0;
 	for (d = alldevs; d != NULL; d = d->next)
@@ -138,11 +209,11 @@ pcap_if_t* choose_device(pcap_if_t* alldevs, pcap_if_t* d, pcap_t* handle, char 
 	for (int j = 1; j < choice && d != NULL; j++)
 		d = d->next;
 
-	system("cls");
-
 	printf("\n****************************************************************************\n\n");
 	printf("      선택된 장치: %s\n", d->name);
 	printf("\n****************************************************************************\n\n");
+
+	system("cls");
 
 	pcap_freealldevs(alldevs);
 
@@ -184,4 +255,13 @@ void print_hex_ascii(const u_char* payload, int len)
 		if (cnt == 64)
 			break;
 	}
+}
+
+int choose_filter(int filter)
+{
+	printf("\n****************************************************************************\n\n");
+	printf("      패킷 유형을 선택하세요. ( [1] IPv4 [2] ARP ) :     ");
+	scanf("%d", &filter);
+
+	return filter;
 }
